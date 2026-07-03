6c0e992c12b3f24262dfc4154533b59529bf8153
AyMi@2025 <ayanmaiti558@gmail.com>
2026-07-04 00:21:08 +0530

    Add Dashboard Components

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ const body = document.querySelector("body");
 const signup = document.querySelector("#signup");
 const login = document.querySelector('login');
 
-const form = document
-
 btn.addEventListener("click", () => {
     if(body.classList.toggle('darkMode')){
         btn.textContent = "☀️";
